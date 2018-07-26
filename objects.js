a258dd3e701b4c765124c2eb738a1bf52ee3875f
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-26 11:30:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {ACDC: 'Hells Bells'};
 
 function updatePlaylist(obj, artist, song){
-  return Object.assign({}, obj, {[key], value})
+  return Object.assign({}, obj, {[key], value});
 }
